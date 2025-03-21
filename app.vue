e37a4cea9969a4003e714508c3ab923b3531ae30
adnanmadi417 <adnanmadi417@gmail.com>
2025-03-21 23:02:09 +0800

    changed color in app

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <nuxt-page/>
+</template>
+<script setup lang="ts">
+</script>
+
+
+<style>
+:root {
+  --primary-color: #00796B;
+  --primary-hover: #00574B;
+  --text-light: #FFFFFF;
+  --text-hover: #B71C1C;
+}
+
+
+</style>
