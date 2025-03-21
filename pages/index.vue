04957619a13e516ef535e57ceecae4b8dae36ec0
adnanmadi417 <adnanmadi417@gmail.com>
2025-03-22 03:27:10 +0800

    remove image bg

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 
 import Footer from "~/component/Footer.vue";
+import Header from "~/component/Header.vue";
+import Landing from "~/component/Landing.vue";
 </script>
 
 <template>
+  <Header/>
+  <Landing/>
   <Footer/>
 </template>
 
